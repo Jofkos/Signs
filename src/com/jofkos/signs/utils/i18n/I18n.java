1068d5b9bed1dd8b29f7a36942d510459c3c245d
Jofkos <JofkosDE@gmail.com>
2015-03-08 15:13:13 +0100

    Removed custom resourceBundle fallback control

--- src/com/jofkos/signs/utils/i18n/I18n.java
+++ src/com/jofkos/signs/utils/i18n/I18n.java
@@ -11,7 +11,7 @@ public class I18n {
 	private static ResourceBundle localeBundle;
 	
 	public static void load() {
-		localeBundle = ResourceBundle.getBundle("messages", Config.LOCALE, new FileClassloader(Signs.class.getClassLoader()), FallbackControl.CONTROL);
+		localeBundle = ResourceBundle.getBundle("messages", Config.LOCALE, new FileClassloader(Signs.class.getClassLoader()));
 	}
 	
 	public static String _(String string, Object... objs) {
