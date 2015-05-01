ed026a7a2ec5c46264333c3475b9b6b50472e833
Jofkos <JofkosDE@gmail.com>
2015-05-01 16:52:51 +0200

    NoCheatPlus' "anti AutoSign" quickfix

---
+++ src/com/jofkos/signs/plugin/NoCheatPlusPlugin.java
@@ -0,0 +1,29 @@
+package com.jofkos.signs.plugin;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+
+import com.jofkos.signs.utils.API;
+import com.jofkos.signs.utils.Utils;
+
+import fr.neatmonster.nocheatplus.checks.blockplace.BlockPlaceData;
+import fr.neatmonster.nocheatplus.checks.blockplace.BlockPlaceListener;
+
+public class NoCheatPlusPlugin extends API.APIPlugin {
+
+	static {
+		clazz = "fr.neatmonster.nocheatplus.NoCheatPlus";
+	}
+
+	@Override
+	public boolean canBuild(Player player, Block block) {
+		BlockPlaceData blockData = BlockPlaceData.getData(player);
+		
+		if (Utils.isSign(block)) {
+			blockData.autoSignPlacedTime = System.currentTimeMillis();
+			blockData.autoSignPlacedHash = BlockPlaceListener.getBlockPlaceHash(block, Material.SIGN);
+		}
+		return true;
+	}
+}
